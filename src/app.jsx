6b7ccbd83eca9fd8c5f83cedc495c4b953bbd848
owstork94 <70260553+owstork94@users.noreply.github.com>
2021-07-08 16:08:50 +0900

    add youtubeservice

--- src/app.jsx
+++ src/app.jsx
@@ -3,37 +3,49 @@ import styles from './app.module.css';
 import Video_list from './components/video_list/video_list';
 import Video_search from './components/video_search_header/video_search_header';
 
-function App() {
+function App({youtube}) {
   const [videos, setvideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+
+    youtube
+    .search(query)
+    .then(videos => setvideos(videos))
+  }
+  //   const requestOptions = {
+  //     method: 'GET',
+  //     redirect: 'follow'
+  //   };
     
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY`, requestOptions)
-      .then(response => response.json())
-      .then(result => 
-        result.items.map(item => ({...item, id: item.id.videoId}))
-      )
-      .then(items => setvideos(items))
-      .catch(error => console.log('error', error));
-  };
+  //   fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY`, requestOptions)
+  //     .then(response => response.json())
+  //     .then(result => 
+  //       result.items.map(item => ({...item, id: item.id.videoId}))
+  //     )
+  //     .then(items => setvideos(items))
+  //     .catch(error => console.log('error', error));
+  // };
+
 
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY", 
-    requestOptions)
-      .then(response => response.json())
-      .then(result => setvideos(result.items))
-      .catch(error => console.log('error', error));
+   youtube
+   .mostPupular()
+   .then(videos => setvideos(videos))
   },[]);
 
-  // onClick(()=>{
+   // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    // };
+    
+    // fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDeMl38hP-K43MGTv6oNhPx5BepglIltbY", 
+    // requestOptions)
+    //   .then(response => response.json())
+    //   .then(result => setvideos(result.items))
+    //   .catch(error => console.log('error', error));
+    
+//////////////////////////////////////////////////////////////////////////////
+ 
+// onClick(()=>{
 
   //   var requestOptions = {
 
